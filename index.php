7e664d84527328089b05923fc2b972242e0a94ab
chabberwock <notengine@gmail.com>
2015-12-09 04:51:14 +0300

    remove debug break

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-DebugBreak();
 
 require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
