854472d47e5de9837c1e69f6b69583f1bd929b1c
chabberwock <notengine@gmail.com>
2015-12-13 09:37:48 +0300

    frontpage: info page

--- plugins/halo/frontpage/admin/views/default/index.php
+++ plugins/halo/frontpage/admin/views/default/index.php
@@ -1,6 +1,13 @@
-<div class="frontpage-default-index">
-    <h1>Basic frontpage module</h1>
-    <p>
-        I'm admin controller
-    </p>
+<?php
+$this->title = 'Frontpage';
+?>
+
+<div class="callout callout-success">
+    <h4><i class="fa fa-info"></i> Current frontpage route </h4>
+    <p><?= Yii::$app->defaultRoute ?></p>
 </div>
+
+<p> Frontpage is controlled by plugins. If you are developer, use this code to set frontpage:</p>
+<pre>
+    Yii::$app->getModule('halo.frontpage')->setRoute('your route');
+</pre>
