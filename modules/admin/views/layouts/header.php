ada74c1464667870e8b2364abe7ee96918eb5a08
chabberwock <notengine@gmail.com>
2015-12-16 14:48:51 +0300

    user panel widget for admin

--- modules/admin/views/layouts/header.php
+++ modules/admin/views/layouts/header.php
@@ -1,6 +1,7 @@
 <?php
 use yii\helpers\Html;
 use admin\widgets\HeaderMenu;
+use admin\widgets\UserPanel;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -19,58 +20,8 @@ use admin\widgets\HeaderMenu;
         <div class="navbar-custom-menu">
 
             <?= HeaderMenu::widget() ?>
+            <?= UserPanel::widget() ?>
             
-            <ul class="nav navbar-nav">
-                <!-- User Account: style can be found in dropdown.less -->
-                <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="user-image" alt="User Image"/>
-                        <span class="hidden-xs">Alexander Pierce</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <!-- User image -->
-                        <li class="user-header">
-                            <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle"
-                                 alt="User Image"/>
-
-                            <p>
-                                Alexander Pierce - Web Developer
-                                <small>Member since Nov. 2012</small>
-                            </p>
-                        </li>
-                        <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Followers</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Sales</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
-                        </li>
-                        <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
-                            <div class="pull-right">
-                                <?= Html::a(
-                                    'Sign out',
-                                    ['/site/logout'],
-                                    ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
-                                ) ?>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-
-                <!-- User Account: style can be found in dropdown.less -->
-                <li>
-                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                </li>
-            </ul>
         </div>
     </nav>
 </header>
---
+++ modules/admin/widgets/views/userpanel/index.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\helpers\Html;
+                      
+?> 
+
+            <ul class="nav navbar-nav">
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="http://gravatar.com/avatar/<?= $user->profile->gravatar_id ?>?s=160" alt="User Image" class="user-image" />
+                        <span class="hidden-xs"><?= $user->username ?></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                        <img src="http://gravatar.com/avatar/<?= $user->profile->gravatar_id ?>?s=160" alt="User Image" class="img-circle" />
+                            <p>
+                                <?= $user->username ?>
+                            </p>
+                        </li>
+                        <!-- Menu Body -->
+                        <!--
+                        <li class="user-body">
+                            <div class="col-xs-4 text-center">
+                                <a href="#">Followers</a>
+                            </div>
+                            <div class="col-xs-4 text-center">
+                                <a href="#">Sales</a>
+                            </div>
+                            <div class="col-xs-4 text-center">
+                                <a href="#">Friends</a>
+                            </div>
+                        </li>
+                        -->
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <?= Html::a('Profile', ['/user/admin/update','id'=>$user->id], ['class'=>'btn btn-default btn-flat']) ?>
+                            </div>
+                            <div class="pull-right">
+                                <?= Html::a(
+                                    'Sign out',
+                                    ['/user/security/logout'],
+                                    ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
+                                ) ?>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- User Account: style can be found in dropdown.less -->
+                <li>
+                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                </li>
+            </ul>
