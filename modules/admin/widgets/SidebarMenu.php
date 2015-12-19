4a571edd148b979419217a35638b8c86c4c1f0df
chabberwock <notengine@gmail.com>
2015-12-19 14:51:05 +0300

    ui fixes

--- modules/admin/widgets/SidebarMenu.php
+++ modules/admin/widgets/SidebarMenu.php
@@ -16,5 +16,41 @@ class SidebarMenu extends Menu
     {
         $this->items = isset($this->view->params['sidebarMenu']) ? $this->view->params['sidebarMenu'] : [];
     }
+    
+    protected function isItemActive($item)
+    {
+        if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+            $route = $item['url'][0];
+            if ($route[0] !== '/' && Yii::$app->controller) {
+                $route = Yii::$app->controller->module->getUniqueId() . '/' . $route;
+            }
+            $arrayRoute = explode('/', ltrim($route, '/'));
+            $arrayThisRoute = explode('/', $this->route);
+            if ($arrayRoute[0] !== $arrayThisRoute[0]) {
+                return false;
+            }
+            if (isset($arrayRoute[1]) && $arrayRoute[1] !== $arrayThisRoute[1]) {
+                return false;
+            }
+            if (isset($arrayRoute[2]) && $arrayRoute[2] !== $arrayThisRoute[2]) {
+                return false;
+            }
+            if (isset($arrayRoute[3]) && $arrayRoute[3] !== $arrayThisRoute[3]) {
+                return false;
+            }
+
+            unset($item['url']['#']);
+            if (count($item['url']) > 1) {
+                foreach (array_splice($item['url'], 1) as $name => $value) {
+                    if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] != $value)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    
 }  
 ?>
