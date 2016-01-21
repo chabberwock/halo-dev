b8918bcb1ae87aa10a6b8fae8061fc19d124cd24
chabberwock <notengine@gmail.com>
2016-01-22 01:14:46 +0300

    frontpage handlers

---
+++ plugins/halo/frontpage/events/FrontpageHandlers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @author Alexandr Makarov
+* Email: notengine@gmail.com
+*/
+namespace halo\frontpage\events;
+
+class FrontpageHandlers extends \yii\base\Event
+{
+    public $handlers = [];
+}
+  
+?>
