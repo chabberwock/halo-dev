474344ee6bba0f15caaee53b1303df2c60945246
chabberwock <notengine@gmail.com>
2016-01-24 00:15:03 +0300

    contaxt page does not need admin menu links

--- plugins/halo/contact/Bootstrap.php
+++ plugins/halo/contact/Bootstrap.php
@@ -8,9 +8,9 @@ class Bootstrap implements BootstrapInterface
 {
     public function bootstrap($app)
     {
-        $app->on('admin.mainMenu', ['halo\contact\admin\Module','onAdminMainMenu']);
-        $app->on('admin.contentMenu', ['halo\contact\admin\Module','onAdminContentMenu']);
-        $app->on('admin.settingsMenu', ['halo\contact\admin\Module','onAdminSettingsMenu']);
+        //$app->on('admin.mainMenu', ['halo\contact\admin\Module','onAdminMainMenu']);
+        //$app->on('admin.contentMenu', ['halo\contact\admin\Module','onAdminContentMenu']);
+        //$app->on('admin.settingsMenu', ['halo\contact\admin\Module','onAdminSettingsMenu']);
     }
     
 }
