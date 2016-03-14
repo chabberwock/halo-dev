3c2633e52260bf08e0a5cb4a86095483cbb4515b
phplego <phplego@gmail.com>
2016-03-14 11:52:35 +0300

    виджет меню проверяет, существует ли таблица (для инсталляции)

--- plugins/halo/menu/widgets/Nav.php
+++ plugins/halo/menu/widgets/Nav.php
@@ -7,6 +7,7 @@
 namespace halo\menu\widgets;
 
 use halo\menu\models\MenuItem;
+use Yii;
 use yii\helpers\ArrayHelper;
 
 class Nav extends \yii\bootstrap\Nav
@@ -16,7 +17,16 @@ class Nav extends \yii\bootstrap\Nav
     public function init()
     {
         parent::init();
+
+        $tableSchema = Yii::$app->db->schema->getTableSchema(MenuItem::tableName());
+
+        if($tableSchema === null)
+        {
+            return;
+        }
+
         $models = MenuItem::find()->where(['menu_id'=>$this->menu])->orderBy(['order_id'=>SORT_ASC])->all();
+
         $items = [];
         // top menu items
         foreach ($models as $model) {
@@ -24,13 +34,13 @@ class Nav extends \yii\bootstrap\Nav
                 $items[$model->id] = ['label'=>$model->title, 'url'=>$this->parseRoute($model->route)];
             }
         }
-        
+
         foreach ($models as $model) {
             if (isset($items[$model->parent_id])) {
                 $items[$model->parent_id]['items'][] = ['label'=>$model->title, 'url'=>$this->parseRoute($model->route)];
             }
         }
-        $this->items = ArrayHelper::merge($items, $this->items);          
+        $this->items = ArrayHelper::merge($items, $this->items);
     }
     
     private function parseRoute($route)
